dab762106e56223c49a53ab13adfa80edc6702dc
binika9 <51786462+binika9@users.noreply.github.com>
2024-03-12 10:32:48 +0300

    Add files via upload

---
+++ Kotlin1/src/KotlinOne.kt
@@ -0,0 +1,45 @@
+fun main(args:Array<String>){
+    //INTEGER
+    //STRING
+    //Double(decimal/float)
+    //Char(single letter)
+    //Boolean
+    var x:Int
+    var y:String
+    var z:Double
+    var a:Char
+    var b:Boolean
+    var f = 34//equating directly
+    var d = 16
+    var answer = f+d
+    var n = 20
+    var m = 7
+    var answer1 = m*n
+
+    x = 10
+    y = "This is my first Kotlin"
+    z = 30.23
+    a = 'w'
+    b = true
+
+    println(x+z)
+    println(f+d)
+    println("Your summation output is"+answer)
+    println(answer1)
+    println("Your multiplication output is $answer1")//compartination??
+
+    val r: Int//val values cant be changed or reassigned
+    val p:String
+    val q:Double
+
+    r = 34
+    p = "This is my variable"
+    q = 24.5
+    var j = r+q
+
+    println("This is my output"+j)
+
+
+
+
+}
